2d4bf3b99cc64399988507cc9335d9e89db8e6fa
cag2050 <cag2050@163.com>
2018-06-22 15:57:02 +0800

    m

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // 设置indent；此时会使用4个空格，作为缩进
+    // "indent": [4, "space", { "SwitchCase": 1 }]
   },
   parserOptions: {
     parser: 'babel-eslint'
